1ec8b66e522f9f570e5ed466cd7495698b2aa3bf
Thomas Butler <tnbutler@gmail.com>
2016-08-06 15:57:06 -0500

    added #employees loop, allergies loop, last statement

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -18,41 +18,61 @@ def age_correct (age, year_born)
 end
 
 
+puts "How many employees do you want to process?"
+num_employees = gets.chomp.to_i
+
+x=0
+while x<num_employees
+	x+=1
 
 
 
+	puts "What is your name?"
+	name = gets.chomp
 
-puts "What is your name?"
-name = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
-
-puts "What year were you born?"
-year_born = gets.chomp.to_i
-
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = to_boolean(gets.chomp)
-
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = to_boolean(gets.chomp)
+	puts "What year were you born?"
+	year_born = gets.chomp.to_i
 
 
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic = to_boolean(gets.chomp)
 
-if (name=="Drake Cula") || (name=="Tu Fang")
-	puts "Definitely a vampire."
-elsif !age_correct(age, year_born) && !garlic && !insurance
-	puts "Almost certainly a vampire."
-elsif age_correct(age, year_born) && (garlic || insurance)
-	puts "Probably not a vampire."
-elsif !age_correct(age, year_born) && (!garlic || !insurance)
-	puts "Probably a vampire."
-else
-	puts "Results inconclusive."
+	puts "Would you like to enroll in the company’s health insurance?"
+	insurance = to_boolean(gets.chomp)
+
+	allergy = "xyz"
+while allergy != "done"
+	puts "Please enter your allergies. Type 'done' when finished."
+	allergy = gets.chomp.downcase
+
+	if allergy == "sunshine"
+		puts "Probably a vampire."
+		break
+	end
+
 end
 
 
+	if allergy == "sunshine"
+		#do nothing
+	elsif (name=="Drake Cula") || (name=="Tu Fang")
+		puts "Definitely a vampire."
+	elsif !age_correct(age, year_born) && !garlic && !insurance
+		puts "Almost certainly a vampire."
+	elsif age_correct(age, year_born) && (garlic || insurance)
+		puts "Probably not a vampire."
+	elsif !age_correct(age, year_born) && (!garlic || !insurance)
+		puts "Probably a vampire."
+	else
+		puts "Results inconclusive."
+	end
+
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 
 
