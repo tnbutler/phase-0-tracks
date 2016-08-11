6be942c4482795eb8512177069a7f8cb0aa334ec
Thomas Butler <tnbutler@gmail.com>
2016-08-10 17:07:48 -0700

    added ability to change one value

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -23,12 +23,32 @@ client_name = gets.chomp
 puts "Enter client age"
 client_age = gets.chomp
 
+puts "Enter number of children"
+num_children = gets.chomp
 
-decor_hash = {name: client_name, age: client_age}
+puts "Do you like green? (answer 'y' or 'n')"
+green = gets.chomp
+if green=="y"
+	green = true
+else
+	green = false
+end
+
+
+decor_hash = {name: client_name, age: client_age, num_children: num_children, green: green}
 puts decor_hash
 
-puts "Update a value by typing in the key name, and you will be prompted to update the value. Type 'none' to skip"
+puts "Update a value by typing in the key name, and you will be prompted to update the value. Type 'none' to skip."
 
-key_string = gets.chomp
-puts "What is the value for #{key_string}?"
+key_symbol = gets.chomp.to_sym
+puts key_symbol
+
+if key_symbol.to_s == "none"
+	# do nothing
+else
+	puts "What is the value for #{key_symbol}?"
+	value = gets.chomp
+	decor_hash[key_symbol] = value
+	puts decor_hash
+end
 
