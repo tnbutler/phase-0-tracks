7bcf10916149d043e36211f5a175b273e4b788c2
Thomas Butler <tnbutler@gmail.com>
2016-08-12 16:53:42 -0700

    added Puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,12 +1,40 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+  	def fetch(toy)
+    	puts "I brought back the #{toy}!"
+    	toy
+	end
+
+	def speak(num)
+		loop=0
+		until loop >= num
+			puts "Woof"
+			loop+=1
+		end
+	end
+
+	def roll_over
+		puts '*rolls over*'
+	end
+
+	def dog_years(human_years)
+		human_years*7
+	end
+
+	def eat(food)
+		puts "I ate the #{food}"
+	end
+
 
 end
 
 
+
+
 fido = Puppy.new
 fido.fetch("ball")
+fido.speak(4)
+fido.roll_over
+puts "5 human years = #{fido.dog_years(5)} dog years."
+fido.eat("dog food")
+
