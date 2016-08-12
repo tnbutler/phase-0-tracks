5e3388e884833c9f8729ecead8d1fc2a0e491250
Thomas Butler <tnbutler@gmail.com>
2016-08-12 16:30:56 -0700

    added comments for Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+=begin
+CLASS ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Size = small
+Legs = 4
+Fur = short
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Play
+Fetch
+Run
+Eat
+=end
