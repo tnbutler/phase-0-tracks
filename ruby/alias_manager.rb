305e682d0b55dbafd76a6cf6d7deae2250eb7431
Thomas Butler <tnbutler@gmail.com>
2016-08-11 17:24:34 -0700

    added more detailed comments

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,11 +1,20 @@
-#swaps first and last name
+=begin 
+swaps first and last name
+change all chars to lowercase
+partition name by space
+recombine the name, swapping last and first
+=end
 def swap_names (name)
 	first_and_last = name.downcase.partition(' ')
 	first_and_last[2] << ' ' << first_and_last[0]
 end
 
-
+=begin
 # changes vowel to next vowel
+change single letter string into an array
+iterate 0->4 through an array of vowels, and if there is a match, advance to the next vowel
+return this vowel
+=end
 def next_vowel (vowel)
 	vowel_ary = vowel.split('')
 	vowels = "aeiou".split('')
@@ -16,8 +25,10 @@ def next_vowel (vowel)
 		end
 	end
 end
-
+=begin
 # changes consonant to next consonant
+do the same thing as next_vowel, just change the constant array and # of consonants
+=end
 def next_consonant (consonant)
 	consonant_ary = consonant.split('')
 	consonants = "bcdfghjklmnpqrstvwxyz".split('')
@@ -30,7 +41,7 @@ def next_consonant (consonant)
 end
 
 
-# creates new alias by swapping first and last name, then next vowel, and next consonant
+# creates new alias by swapping first and last name, then applying next_vowel, and next_consonant
 def create_alias(name)
 	full_name = swap_names(name)
 	full_name_ary = full_name.split('')
@@ -60,6 +71,13 @@ puts "Enter your name, and alias will be created. Type 'quit' to quit."
 name = gets.chomp
 
 
+
+=begin
+create an empty hash of names (key) and aliases (value)
+unless name == 'quit' then merge the name/alias pair into the hash 
+=end 
+	
+end
 hash_of_names = {}
 until name == "quit"
 	fake_name = create_alias(name)
