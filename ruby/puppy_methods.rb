872698cfaa929170e279a169822b285c9d86ed27
Thomas Butler <tnbutler@gmail.com>
2016-08-12 17:16:26 -0700

    created Cat class with play and litterbox methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -25,16 +25,59 @@ class Puppy
 		puts "I ate the #{food}"
 	end
 
+	def initalize
+		puts "Initializing new Puppy instance."
+	end
 
 end
 
 
+class Cat
+
+	def initialize
+		puts "Initializing new Cat instance"
+	end
+
+	def play(toy)
+		puts "Leave me alone, I am playing with the #{toy}"
+	end
+
+	def litterbox
+		puts "I am going to use the litterbox."
+	end
+
+end
+
+=begin
+Use a loop to make 50 instances of your class.
+Modify your loop so that it stores all of the instances in a data structure.
+Iterate over that data structure using .each and call the instance methods you wrote on each instance. So if you wrote a Gymnast class, this is where you'd call .flip and .jump on each of your instances of Gymnast.
+=end
 
 
+x=0
+cat_ary = []
+
+until x>=50
+	cat = Cat.new
+	cat_ary << cat
+	x+=1
+end
+
+cat_ary.each { |cat_instance|
+	cat_instance.play("yarn")
+	cat_instance.litterbox
+}
+
+# p cat_ary
+
+
+
+=begin
 fido = Puppy.new
 fido.fetch("ball")
 fido.speak(4)
 fido.roll_over
 puts "5 human years = #{fido.dog_years(5)} dog years."
 fido.eat("dog food")
-
+=end
