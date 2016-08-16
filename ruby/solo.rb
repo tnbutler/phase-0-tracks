da839461d485de602fa4c9773e2a5ca202cb9ecf
Thomas Butler <tnbutler@gmail.com>
2016-08-16 10:31:16 -0700

    initialized branch

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+=begin
+
+class therapybot
+	attributes: botname, depressionscore, cashbalance
+
+	methods: do therapy, charge money (money), do meditation
+
+=end
+
+
+
+class Therapy_bot
+	attr_accessor :bot_name, :depression_score
+	attr_reader :cash_balance
